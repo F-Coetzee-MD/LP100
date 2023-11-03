e5e4d6ccf76cae16c3b6afd4e6f1994afa63a1a4
F-Coetzee-MD <fcoetzee@masterdrilling.com>
2023-11-03 13:46:56 +0200

    aids

--- remote_code/source/main_loop.py
+++ remote_code/source/main_loop.py
@@ -5,26 +5,29 @@ import threading
 from modbus_converter import modbus_maker
 from usb_connection import usb_listener
 from tcp_connection import tcp_client
+from timeout_decorator import timeout
 
 usb_port = usb_listener()
 modbus = modbus_maker()
 client = tcp_client()
 
-main_loop_failed = None
+@timeout(2)
+def main_loop():
+    # blocking code, will wait untill a message is received
+    msg = usb_port.wait_for_pycan_message()
+    print(msg)
+    raw_data = usb_port.format_can_data(msg)
+
+    # if data was received
+    if(len(raw_data)):
+        frame = modbus.create_new(raw_data, msg.arbitration_id)
+        client.forward_message(frame)
+
+    client.receive_response()
+
 
 if __name__ == "__main__":
-    while (True):
-        # blocking code, will wait untill a message is received
-        msg = usb_port.wait_for_pycan_message()
-        print(msg)
-        raw_data = usb_port.format_can_data(msg)
-
-        # if data was received
-        if(len(raw_data)):
-            frame = modbus.create_new(raw_data, msg.arbitration_id)
-            client.forward_message(frame)
-
-        client.receive_response()
-        main_loop_failed = False
+    while (True): main_loop()
+        
 
     
--- remote_code/source/monitor.py
+++ remote_code/source/monitor.py
@@ -1,11 +1,10 @@
 import time
 import subprocess
 
-target_script = "your_target_script.py"
-
 while True:
     # Wait for the target script to exit
-    process = subprocess.Popen(["python", target_script], shell=True)
+    process = subprocess.Popen(
+        ["python", "main_loop.py"], shell=True)
     process.wait()  
 
     # Wait for 5 seconds before restarting
