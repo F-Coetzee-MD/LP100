f6a606e61b65b7296c195a033a97158f5074131b
F-Coetzee-MD <fcoetzee@masterdrilling.com>
2023-11-09 16:12:17 +0200

    arno fix

--- remote_code/source/main_loop.py
+++ remote_code/source/main_loop.py
@@ -15,44 +15,42 @@ def timeout_checker():
         
         if (main_loop_error.is_set()):
             break
-        # elif KeyboardInterrupt:
-        #     break
+        #elif KeyboardInterrupt:
+             #break
 
 def main_loop():
-    # usb_port = usb_listener()
+    usb_port = usb_listener()
     modbus = modbus_maker()
     client = tcp_client()
 
     while (True):
-        # # blocking code, will wait untill a message is received
-        # msg = usb_port.wait_for_pycan_message()
-        # print(msg)
-        # raw_data = usb_port.format_can_data(msg)
-
-        raw_data = [0, 100, 0, 0, 0, 23, 1, 16, 28, 92, 0, 8, 16, 0, 127, 0, 127, 0, 127, 0, 127, 0, 127, 0, 127, 0, 127, 0, 127]
+        # blocking code, will wait untill a message is received
+        msg = usb_port.wait_for_pycan_message()
+        print(msg)
+        raw_data = usb_port.format_can_data(msg)
 
         # if data was received
         if(len(raw_data)):
-            # frame = modbus.create_new(raw_data, msg.arbitration_id)
-            frame = modbus.create_new(raw_data, 104)
+            frame = modbus.create_new(raw_data, msg.arbitration_id)
+            # frame = modbus.create_new(raw_data, 104)
             client.forward_message(frame)
 
-        temp = client.receive_response()
-        print([x for x in temp])
+        client.receive_response()
+        # print([x for x in temp])
         main_loop_error.clear()
-        time.sleep(0.05)
+        #ctime.sleep(0.05)
 
 if __name__ == "__main__":
-    # process1 = threading.Thread(target = timeout_checker)
-    # process2 = threading.Thread(target = main_loop)
+    process1 = threading.Thread(target = timeout_checker)
+    process2 = threading.Thread(target = main_loop)
 
-    # process1.daemon = True
-    # process2.daemon = True
+    process1.daemon = True
+    process2.daemon = True
 
-    # process1.start()
-    # process2.start()
+    process1.start()
+    process2.start()
 
-    # process1.join()
-    # print("end it all")
-    # sys.exit()
-    main_loop()
+    process1.join()
+    print("end it all")
+    sys.exit()
+    #main_loop()
